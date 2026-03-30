5a870077ce95bb90963956193ec22d3b3d24720f
yadsun687 <sundaypapa25@gmail.com>
2026-03-30 08:20:01 +0700

    refactor

--- Scripts/Characters/foot_ik.gd
+++ Scripts/Characters/foot_ik.gd
@@ -1,104 +1,198 @@
+@tool
 extends Node3D
-class_name FootIK
 
-@export var skeleton: Skeleton3D
-@export var player_mesh : Node3D
+@export_group("Left")
+@export var l_bone_ik: TwoBoneIK3D
+@export var l_root_bone: String
+@export var l_middle_bone: String
+@export var l_end_bone: String
+@export var l_toe_bone: String
 
-@export_group("right")
-@export var right_target_node: Node3D
-@export var right_pole_node: Node3D
-@export var right_heel_raycast_3d: RayCast3D
-@export var right_toe_raycast_3d: RayCast3D
-@export_group("left")
-@export var left_target_node: Node3D
-@export var left_pole_node: Node3D
-@export var left_heel_raycast_3d: RayCast3D
-@export var left_toe_raycast_3d: RayCast3D
 
-var hips_idx: int
-var right_foot_idx: int
-var right_toe_idx: int
-var right_knee_idx: int
-var left_foot_idx: int
-var left_toe_idx: int
-var left_knee_idx: int
+@export_group("Right")
+@export var r_bone_ik: TwoBoneIK3D
+@export var r_root_bone: String
+@export var r_middle_bone: String
+@export var r_end_bone: String
+@export var r_toe_bone: String
 
-var skeleton_base_y: float
+@onready var l_heel_raycast: RayCast3D = $L_Affector/HeelRayCast3D
+@onready var l_heel_target: Marker3D = $L_Affector/HeelMarker
+@onready var l_toe_raycast: RayCast3D = $L_Affector/ToeRayCast3D
+@onready var l_toe_target: Marker3D = $L_Affector/ToeMarker
+
+@onready var r_heel_raycast: RayCast3D = $R_Affector/HeelRayCast3D
+@onready var r_heel_target: Marker3D = $R_Affector/HeelMarker
+@onready var r_toe_raycast: RayCast3D = $R_Affector/ToeRayCast3D
+@onready var r_toe_target: Marker3D = $R_Affector/ToeMarker
+
+@export_group("Animation Blend")
+@export var animation_player:AnimationPlayer
+@export var blend_speed: float = 100.0
+
+@export_group("Parameters")
+@export_range(0.0,1.0,0.01) var ik_influence:float = 0.6
+## Offset of the heel target from ground
+@export var heel_offset:float = 0.0
+
+var skeleton: Skeleton3D
+
+var l_root_bone_idx: int
+var l_middle_bone_idx: int
+var l_end_bone_idx: int
+var l_toe_bone_idx: int
+
+var r_root_bone_idx: int
+var r_middle_bone_idx: int
+var r_end_bone_idx: int
+var r_toe_bone_idx: int
+
+@export var l_foot_max_height: float = 0.25
+@export var r_foot_max_height: float = 0.25
+
+# @export var player_mesh : Node3D
+
+# @export_group("other")
+# @export var animation_tree : AnimationTree
+# @export var active_at_ground: bool = true
+
+# var hips_idx: int
+
+# var skeleton_base_y: float
+
+# var left_ik_weight:float = 0.0
+# var right_ik_weight:float = 0.0
+# var foot_lift_height :float = 0.3
+
+func _validate_property(property: Dictionary) -> void:
+	if property.type == TYPE_STRING and property.name.ends_with("_bone"):
+		if skeleton:
+			property.hint = PROPERTY_HINT_ENUM
+			property.hint_string = skeleton.get_concatenated_bone_names()
+
+func _get_configuration_warnings():
+	var warnings = []
+
+	if (skeleton == null) or not (skeleton is Skeleton3D):
+		warnings.append("Missing parent, please place the node inside Skeleton3D node")
+
+	return warnings
+
+func _enter_tree():
+	# Find parent Skeleton3D
+	var parent_node = get_parent()
+	assert((parent_node != null) and (parent_node is Skeleton3D) , "Parent is not a Skeleton3D")
+	skeleton = parent_node
+	print("Detected parent Skeleton3D: ", skeleton.name)
+
+	notify_property_list_changed()
 
 func _ready():
-	skeleton_base_y = skeleton.position.y
-	hips_idx = skeleton.find_bone("Hips")
-	right_foot_idx = skeleton.find_bone("RightFoot")
-	right_toe_idx = skeleton.find_bone("RightToeBase")
-	right_knee_idx = skeleton.find_bone("RightLeg")
-	left_foot_idx = skeleton.find_bone("LeftFoot")
-	left_toe_idx = skeleton.find_bone("LeftToeBase")
-	left_knee_idx = skeleton.find_bone("LeftLeg")
+	l_root_bone_idx = skeleton.find_bone(l_root_bone)
+	l_middle_bone_idx = skeleton.find_bone(l_middle_bone)
+	l_end_bone_idx = skeleton.find_bone(l_end_bone)
+	l_toe_bone_idx = skeleton.find_bone(l_toe_bone)
 
-func _process(delta):
-	var right_offset = get_foot_offset(right_foot_idx, right_heel_raycast_3d)
-	var left_offset = get_foot_offset(left_foot_idx, left_heel_raycast_3d)
-	apply_hip_offset(min(right_offset, left_offset))
-	
-	solver(right_foot_idx, right_toe_idx, right_knee_idx, right_heel_raycast_3d, right_toe_raycast_3d, right_pole_node, right_target_node)
-	solver(left_foot_idx, left_toe_idx, left_knee_idx, left_heel_raycast_3d, left_toe_raycast_3d, left_pole_node, left_target_node)
+	r_root_bone_idx = skeleton.find_bone(r_root_bone)
+	r_middle_bone_idx = skeleton.find_bone(r_middle_bone)
+	r_end_bone_idx = skeleton.find_bone(r_end_bone)
+	r_toe_bone_idx = skeleton.find_bone(r_toe_bone)
+	assign_bone_ik_param()
+
+func _physics_process(delta: float) -> void:
+	if Engine.is_editor_hint():
+		return
+	update_raycast()
+	# update_ik_influence(delta)
+	update_affector()
+
+	# Called after IK is solved
+	update_feet_rotation()
 	draw_gizmos()
 
-func solver(foot_bone_idx: int, toe_bone_idx: int, knee_bone_idx: int, heel_raycast: RayCast3D, toe_raycast: RayCast3D, pole_node: Node3D, target_node: Node3D):
-	var f_bone_local = skeleton.get_bone_global_pose(foot_bone_idx)
-	var f_bone_world = skeleton.global_transform * f_bone_local
-	heel_raycast.global_position = f_bone_world.origin
 
-	var t_bone_local = skeleton.get_bone_global_pose(toe_bone_idx)
-	var t_bone_world = skeleton.global_transform * t_bone_local
-	toe_raycast.global_position = t_bone_world.origin
+func assign_bone_ik_param():
+	"""
+	Set corresponding value to both TwoBoneIK3D (left/right)
+	"""
+	l_bone_ik.set_target_node(0,l_heel_target.get_path())
+	l_bone_ik.set_root_bone(0,l_root_bone_idx)
+	l_bone_ik.set_middle_bone(0,l_middle_bone_idx)
+	l_bone_ik.set_end_bone(0,l_end_bone_idx)
+	l_bone_ik.influence = ik_influence
 
-	var k_bone_world = skeleton.global_transform * skeleton.get_bone_global_pose(knee_bone_idx)
-	pole_node.global_position = k_bone_world.origin + player_mesh.global_basis.z * 0.5
-	
-	if heel_raycast.is_colliding() and toe_raycast.is_colliding():
-		var heel_hit = heel_raycast.get_collision_point()
-		var toe_hit = toe_raycast.get_collision_point()
+	r_bone_ik.set_target_node(0,r_heel_target.get_path())
+	r_bone_ik.set_root_bone(0,r_root_bone_idx)
+	r_bone_ik.set_middle_bone(0,r_middle_bone_idx)
+	r_bone_ik.set_end_bone(0,r_end_bone_idx)
+	r_bone_ik.influence = ik_influence
 
-		DebugDraw3D.draw_sphere(heel_hit, 0.05, Color.YELLOW)
-		DebugDraw3D.draw_sphere(f_bone_world.origin, 0.05, Color.WHITE)
-	
-		# surface normal from heel
-		var ground_normal = heel_raycast.get_collision_normal()
-	
-		# forward direction = from heel to toe
-		var forward = (toe_hit - heel_hit).normalized()
-	
-		# recalculate right axis to be perpendicular to both
-		var right = forward.cross(ground_normal).normalized()
-	
-		# rebuild forward to be clean
-		forward = ground_normal.cross(right).normalized()
-	
-		target_node.global_basis = Basis(right, ground_normal, -forward)
-		target_node.global_position = heel_hit
+func update_ik_influence(delta:float):
+	var l_end_bone_y = get_foot_anim_height(l_end_bone_idx)
+	var r_end_bone_y = get_foot_anim_height(l_end_bone_idx)
 
-func get_foot_offset(foot_bone_idx: int, heel_raycast: RayCast3D) -> float:
-	if not heel_raycast.is_colliding():
-		return 0.0
-	var f_bone_world = skeleton.global_transform * skeleton.get_bone_global_pose(foot_bone_idx)
-	return heel_raycast.get_collision_point().y - f_bone_world.origin.y
+	# How high is the animated foot above ground
+	var l_height = l_end_bone_y - l_heel_raycast.get_collision_point().y
+	var r_height = r_end_bone_y - r_heel_raycast.get_collision_point().y
 
-func apply_hip_offset(offset: float):
-	print("offset: ", offset, " skeleton.y: ", skeleton.position.y)
-	var target_y = skeleton_base_y  + offset
-	skeleton.position.y = lerpf(skeleton.position.y, target_y, 0.1)
+	var l_target = 1.0 - clampf(l_height / l_foot_max_height, 0.0, 1.0)
+	var r_target = 1.0 - clampf(r_height / r_foot_max_height, 0.0, 1.0)
+
+	l_bone_ik.influence = move_toward(l_bone_ik.influence, l_target, delta * blend_speed)
+	r_bone_ik.influence = move_toward(r_bone_ik.influence, r_target, delta * blend_speed)
+
+func update_raycast():
+	"""
+	update raycast position to follow skeleton's heel bone
+	"""
+	var l_end_bone_local = skeleton.get_bone_global_pose(l_end_bone_idx)
+	var l_end_bone_world = skeleton.global_transform * l_end_bone_local
+	l_heel_raycast.global_position = l_end_bone_world.origin + (Vector3.UP * 0.4)
+
+	var r_end_bone_local = skeleton.get_bone_global_pose(r_end_bone_idx)
+	var r_end_bone_world = skeleton.global_transform * r_end_bone_local
+	r_heel_raycast.global_position = r_end_bone_world.origin + (Vector3.UP * 0.4)
+
+
+	var l_toe_bone_local = skeleton.get_bone_global_pose(l_toe_bone_idx)
+	var l_toe_bone_world = skeleton.global_transform * l_toe_bone_local
+	l_toe_raycast.global_position = l_toe_bone_world.origin + (Vector3.UP * 0.4)
+
+	var r_toe_bone_local = skeleton.get_bone_global_pose(r_toe_bone_idx)
+	var r_toe_bone_world = skeleton.global_transform * r_toe_bone_local
+	r_toe_raycast.global_position = r_toe_bone_world.origin + (Vector3.UP * 0.4)
+
+func update_affector():
+	"""
+	update affector to ground below feet
+	"""
+	var heel_offset_vec = Vector3.UP * heel_offset
+
+	if l_heel_raycast.is_colliding():
+		var l_heel_hit = l_heel_raycast.get_collision_point()
+		l_heel_target.global_position = l_heel_hit + heel_offset_vec
+
+	if r_heel_raycast.is_colliding():
+		var r_heel_hit = r_heel_raycast.get_collision_point()
+		r_heel_target.global_position = r_heel_hit + heel_offset_vec
+
+func get_foot_anim_height(bone_idx: int) -> float:
+	# This is the bone position from animation, before IK
+	var bone_pose = skeleton.global_transform * skeleton.get_bone_global_pose(bone_idx)
+	return bone_pose.origin.y
 
 func draw_gizmos():
 	# for easier debugging during play test in godot
-	DebugDraw3D.draw_sphere(right_target_node.global_position, 0.03, Color.BLUE)
-	DebugDraw3D.draw_sphere(left_target_node.global_position, 0.03, Color.RED)
+	DebugDraw3D.draw_sphere(l_heel_target.global_position, 0.03, Color.BLUE)
+	DebugDraw3D.draw_sphere(r_heel_target.global_position, 0.03, Color.RED)
 
-	DebugDraw3D.draw_sphere(right_pole_node.global_position, 0.03, Color.DEEP_SKY_BLUE)
-	DebugDraw3D.draw_sphere(left_pole_node.global_position, 0.03, Color.DEEP_PINK)
+	# DebugDraw3D.draw_sphere(right_pole_node.global_position, 0.03, Color.DEEP_SKY_BLUE)
+	# DebugDraw3D.draw_sphere(left_pole_node.global_position, 0.03, Color.DEEP_PINK)
 
-	DebugDraw3D.draw_ray(right_heel_raycast_3d.global_position, Vector3.DOWN, -right_heel_raycast_3d.target_position.y, Color.GREEN)
-	DebugDraw3D.draw_ray(right_toe_raycast_3d.global_position, Vector3.DOWN, -right_toe_raycast_3d.target_position.y, Color.GREEN)
+	DebugDraw3D.draw_ray(l_heel_raycast.global_position, Vector3.DOWN, -l_heel_raycast.target_position.y, Color.ORANGE)
+	DebugDraw3D.draw_ray(l_toe_raycast.global_position, Vector3.DOWN, -l_toe_raycast.target_position.y, Color.GREEN)
 
-	DebugDraw3D.draw_ray(left_heel_raycast_3d.global_position, Vector3.DOWN, -left_heel_raycast_3d.target_position.y, Color.ORANGE)
-	DebugDraw3D.draw_ray(left_toe_raycast_3d.global_position, Vector3.DOWN, -left_toe_raycast_3d.target_position.y, Color.ORANGE)
+	DebugDraw3D.draw_ray(r_heel_raycast.global_position, Vector3.DOWN, -r_heel_raycast.target_position.y, Color.ORANGE)
+	DebugDraw3D.draw_ray(r_toe_raycast.global_position, Vector3.DOWN, -r_toe_raycast.target_position.y, Color.GREEN)
+# 	skeleton_base_y = skeleton.position.y
+# 	hips_idx = skeleton.find_bone("Hips")
