4028d9d0420d4d3089be3ec032ab44078905fab6
Star2578 <nuttykingger@gmail.com>
2026-04-30 04:11:30 +0700

    test scenes

--- Scripts/Manager.gd
+++ Scripts/Manager.gd
@@ -10,30 +10,37 @@ var ik_status: String = "Baseline (no IK)"
 var test_interface: Test_Interface
 
 func _input(event):
-    if event is InputEvent:
-        if Input.is_action_just_pressed("no_ik"):
-            is_baseline = true
-            is_simple = false
-            is_footik = false
-        if Input.is_action_just_pressed("simple_ik"):
-            is_baseline = false
-            is_simple = true
-            is_footik = false
-        if Input.is_action_just_pressed("foot_ik"):
-            is_baseline = false
-            is_simple = false
-            is_footik = true
+	if event is InputEvent:
+		if Input.is_action_just_pressed("no_ik"):
+			is_baseline = true
+			is_simple = false
+			is_footik = false
+			toggle_ik(is_baseline, is_simple, is_footik)
+		if Input.is_action_just_pressed("simple_ik"):
+			is_baseline = false
+			is_simple = true
+			is_footik = false
+			toggle_ik(is_baseline, is_simple, is_footik)
+		if Input.is_action_just_pressed("foot_ik"):
+			is_baseline = false
+			is_simple = false
+			is_footik = true
+			toggle_ik(is_baseline, is_simple, is_footik)
 
 func _process(_delta):
 
-    match true:
-        is_baseline:
-            ik_status = "Baseline (no IK)"
-        is_simple:
-            ik_status = "Simple"
-        is_footik:
-            ik_status = "FootIKModifier"
-        _:
-            ik_status = "Err"
-    
-    test_interface.update_status()
+	match true:
+		is_baseline:
+			ik_status = "Baseline (no IK)"
+		is_simple:
+			ik_status = "Simple"
+		is_footik:
+			ik_status = "FootIKModifier"
+		_:
+			ik_status = "Err"
+	
+	test_interface.update_status()
+
+func toggle_ik(baseline: bool, simple: bool, footik: bool):
+	## TODO: toggle ik
+	pass
